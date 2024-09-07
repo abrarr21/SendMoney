ac326f29ccc2ecfe9cc41dcca8f1554591554c54
abrarr21 <abrarrr2110@gmail.com>
2024-09-07 21:28:20 +0530

    removed imp data

--- backend/constant.js
+++ backend/constant.js
@@ -1,3 +1,2 @@
-export const JWT_SECRET = "mysecretkey";
-export const MONGODB_URI =
-  "mongodb+srv://admin:Password%402023@cluster0.6dbfm.mongodb.net/";
+export const JWT_SECRET = "";
+export const MONGODB_URI = "";
