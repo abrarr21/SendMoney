83065d8305516c8538951b0b6a239d7dc785f2ff
abrarr21 <abrarrr2110@gmail.com>
2024-09-09 11:21:00 +0530

    functionalities

---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import Button from "./Button";
+import { useEffect } from "react";
+import axios from "axios";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:6969/api/v1/user/bulk?filter=" + filter)
+      .then((response) => {
+        setUsers(response.data.user);
+      });
+  }, [filter]);
+  return (
+    <>
+      {" "}
+      <div className="font-bold mt-6 text-lg">Users</div>
+      <div className="my-2">
+        <input
+          onChange={(e) => setFilter(e.target.value)}
+          type="text"
+          placeholder="Search User..."
+          className="w-full px-2 py-1 border border-slate-200"
+        />
+      </div>
+      <div>
+        {users.map((user) => (
+          <User user={user} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const User = ({ user }) => {
+  return (
+    <div className="flex justify-between">
+      <div className="flex">
+        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+          <div className="flex flex-col justify-center h-full text-xl">
+            {user.firstname[0]}
+          </div>
+        </div>
+        <div className="flex flex-col justify-center h-full">
+          <div>
+            {user.firstname} {user.lastname}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center h-full">
+        <Button label={"Send Money"} />
+      </div>
+    </div>
+  );
+};
+
+export default Users;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,22 +1,59 @@
+import { useState } from "react";
 import BottomWarning from "../components/BottomWarning";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
 import InputBox from "../components/InputBox";
 import Subheading from "../components/Subheading";
+import axios from "axios";
 
 const Signup = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign Up"} />
           <Subheading lable={"Enter your information to create an account."} />
-          <InputBox placeholder="Chris" label={"First Name"} />
-          <InputBox placeholder="Hemsworth" label={"Last Name"} />
-          <InputBox placeholder="iamTHOR@gmail.com" label={"Email"} />
-          <InputBox placeholder="********" label={"Password"} />
+          <InputBox
+            onChange={(e) => setFirstname(e.target.value)}
+            placeholder="Chris"
+            label={"First Name"}
+          />
+          <InputBox
+            onChange={(e) => setLastname(e.target.value)}
+            placeholder="Hemsworth"
+            label={"Last Name"}
+          />
+          <InputBox
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="iamTHOR@gmail.com"
+            label={"Email"}
+          />
+          <InputBox
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="********"
+            label={"Password"}
+          />
           <div className="pt-4">
-            <Button label={"Sign up"} />
+            <Button
+              onClick={async () => {
+                const response = await axios.post(
+                  "http://localhost:6969/api/v1/user/signup",
+                  {
+                    firstname,
+                    lastname,
+                    username,
+                    password,
+                  },
+                );
+                localStorage.setItem("token", response.data.token);
+              }}
+              label={"Sign up"}
+            />
           </div>
           <BottomWarning
             label={"Already have an account! "}
